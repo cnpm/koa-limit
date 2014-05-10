92d858f87276f6d1560e599e1a4e6da1bc2fa679
dead_horse <dead_horse@qq.com>
2014-05-10 22:45:18 +0800

    fix retry-after header into second

--- example.js
+++ example.js
@@ -4,15 +4,15 @@ var favicon = require('koa-favicon');
 var limit = require('./');
 var redis = require('redis');
 var wrapper = require('co-redis');
-var redisClient = require('redis').createClient();
-var client = wrapper(redisClient);
+// var redisClient = require('redis').createClient();
+// var client = wrapper(redisClient);
 
 var app = koa();
 app.use(favicon());
 app.use(limit({
   limit: 3,
-  interval: 10000
-  store: client
+  interval: 100000,
+  // store: client
 }));
 
 app.use(function *() {
